d2e87ea5a06f9a5a651e21abf68b5a63f77b0687
Your Name <you@example.com>
2025-05-08 19:27:51 -0300

    removendo tudo

--- infra/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-tfstate"
-    storage_account_name = "tfstatecurso10998"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
